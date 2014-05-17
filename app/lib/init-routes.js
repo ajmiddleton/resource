15929b82d93f97b7c98f0c476b4c44a9671ec7b6
Andrew Middleton <middleton.andrew.j@gmail.com>
2014-05-17 12:11:32 -0500

    init

---
+++ app/lib/init-routes.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var traceur = require('traceur');
+var dbg = traceur.require(__dirname + '/route-debugger.js');
+var initialized = false;
+
+module.exports = (req, res, next)=>{
+  if(!initialized){
+    initialized = true;
+    load(req.app, next);
+  }else{
+    next();
+  }
+};
+
+function load(app, fn){
+  // var home = traceur.require(__dirname + '/../routes/home.js');
+  var game = traceur.require(__dirname + '/../routes/game.js');
+  var users = traceur.require(__dirname + '/../routes/users.js');
+
+  app.get('/', dbg, game.index);
+
+  app.post('/login', dbg, users.login);
+
+
+  console.log('Routes Loaded');
+  fn();
+}
